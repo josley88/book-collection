47a164f6f6f1fec0b06464b4b08f0dee78e54d2a
root <root@localhost>
2022-09-17 23:35:10 +0000

    Updated db seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    has_many :books, through: :user_book:scale => 
+    has_many :books, through: :user_books
     has_many :user_books
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+    {
+        title: "Harry Potter and the Philosopher's Stone", 
+        author: "JK Rowling", 
+        price: "1.0", 
+        publicationDate: "2022-09-17"
+    }
+])
+
+User.create!([
+  {username: "joseph"}
+])
+
+UserBook.create!([
+  {user_id: 1, book_id: 1}
+])
